6fd0ecf99a01b33f5e163ab5a73158bf936cc2b7
Marcus Rådell <marcus@radell.net>
2023-12-20 08:23:22 +0100

    add input field for setting the name

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 const greeting = "Hello, Davit!";
 
 function App() {
-
+  const [name, setName] = useState("");
 
   return (
     <>
       <h1>{greeting}</h1>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
     </>
   );
 }
